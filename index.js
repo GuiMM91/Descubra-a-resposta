0f97822fdbade4dbac661f9fbf9c386c17963eea
Guilherme Marques Mesquita <g.m.mesquita@hotmail.com>
2022-06-17 16:43:33 -0300

    feat: new repo

---
+++ index.js
@@ -0,0 +1,58 @@
+const elemetoResposta = document.querySelector("#resposta");
+const inputPergunta = document.querySelector("#inputPergunta");
+const buttonPerguntar = document.querySelector("#buttonPerguntar");
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+];
+
+function fazerPergunta() {
+  if (inputPergunta.value.includes("?")) {
+    console.log("show");
+  } else {
+    alert("Você realmente quer me perguntar algo?");
+    return;
+  }
+
+  if (inputPergunta.value === "?") {
+    alert("Você não esqueceu de algo?");
+    return;
+  }
+
+  if (inputPergunta.value == "") {
+    alert("Digite sua pergunta");
+    return;
+  }
+
+  buttonPerguntar.setAttribute("disabled", true);
+  const pergunta = "<div>" + inputPergunta.value + "</div>";
+
+  const totalRespostas = respostas.length;
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas);
+
+  console.log(numeroAleatorio);
+  elemetoResposta.innerHTML = pergunta + respostas[numeroAleatorio];
+
+  elemetoResposta.style.opacity = 1;
+  setTimeout(function () {
+    elemetoResposta.style.opacity = 0;
+    buttonPerguntar.removeAttribute("disabled");
+  }, 3000);
+}
